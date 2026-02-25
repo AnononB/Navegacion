45924b45c50937467fecf855e056082dfac80360
Ricardo García <hazelgrmtz8@gmail.com>
2026-02-25 08:55:17 -0600

    Se ajusto solamente un fill para el tamaño de la lista

--- app/src/main/java/com/example/navigationr/view/ListScreen.kt
+++ app/src/main/java/com/example/navigationr/view/ListScreen.kt
@@ -2,6 +2,7 @@ package com.example.navigationr.view
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,7 +17,7 @@ fun ListScreen (navController: NavController){
     val personas = listOf("Goytia","Villa","Memo Blanco", "Isael", "Ing. Mota")
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxWidth(),
 
     )
     {
@@ -26,7 +27,7 @@ fun ListScreen (navController: NavController){
            Text(
 
                text = persona,
-               modifier = Modifier.fillMaxSize().clickable {
+               modifier = Modifier.fillMaxWidth().clickable {
                    navController.navigate("details/$persona")
                }.padding(16.dp)
            )
